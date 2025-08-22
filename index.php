9ae9abb3f0cb2c8ad7b70b488b6ea2a73dfb982f
Thorvaldur Konradsson <tobbetto@users.noreply.github.com>
2025-08-22 08:30:42 +0000

    fix js

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Dashboard main page
+ * Dashboard plugin for Moodle
  *
- * @package    local_cuadrodemando
+ * @package    local_cuadrodemando  
  * @author     Thorvaldur Konradsson
  * @version    1.0.0
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -31,6 +31,19 @@ $PAGE->set_url('/local/cuadrodemando/index.php');
 $PAGE->set_title(get_string('dashboard', 'local_cuadrodemando'));
 $PAGE->set_heading(get_string('dashboard', 'local_cuadrodemando'));
 $PAGE->set_pagelayout('admin');
+
+// Load CSS and JavaScript libraries
+$PAGE->requires->css('/local/cuadrodemando/thirdpartylibs/fontawesome/css/all.min.css');
+
+// Load Chart.js from your thirdpartylibs
+$PAGE->requires->js('/local/cuadrodemando/thirdpartylibs/chart/chart.min.js');
+
+// Load jQuery UI for sortable widgets (if you want dashboard widgets to be sortable)
+$PAGE->requires->js('/local/cuadrodemando/thirdpartylibs/jquery/jquery-ui.min.js');
+$PAGE->requires->css('/local/cuadrodemando/thirdpartylibs/jquery/jquery-ui.css');
+
+// Load any custom CSS for your dashboard
+$PAGE->requires->css('/local/cuadrodemando/thirdpartylibs/adminlte/adminlte.min.css');
 
 // Display the dashboard
 \local_cuadrodemando\dashboard_controller::display_dashboard();
